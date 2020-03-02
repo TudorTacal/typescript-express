a9ddca0f610c343717f92119e86d8a4de075b1bf
TudorTacal <tudortacal@gmail.com>
2020-03-02 10:25:19 +0000

    project setup - create login route - add session

---
+++ server/src/routes/loginRoutes.ts
@@ -0,0 +1,36 @@
+import { Router, Request, Response } from 'express';
+
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined }
+}
+
+const router = Router();
+
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+    <form method="POST">
+      <div>
+        <label>Email</label>
+        <input name="email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input name="password" type="password" />
+      </div>
+      <button>Submit</button>
+    </form>
+  `);
+});
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+  const { email, password } = req.body;
+  if (email && password && email === 'tudortacal@gmail.com' && password === 'password') {
+    // mark this person as logged in
+    req.session = { loggedIn: true };
+    res.redirect('/');
+  } else {
+    res.send('Invalid email or password');
+  }
+});
+
+export { router };
